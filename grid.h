58ec7cb31821bb7e3d33090900e1479f2e6ea9fa
EldieSoft <150984579+EldieSoft@users.noreply.github.com>
2024-02-24 15:16:22 -0500

    Update grid.h

--- grid.h
+++ grid.h
@@ -1,10 +1,19 @@
 /*grid.h*/
 #ifndef PRIMARY_HEADER_H
 #define PRIMARY_HEADER_H
-
+#define MAX_POINTS 10
 #include <stdio.h>
 
-void print_grid(int w, int h);
+typedef struct points{
+	int x;
+	int y;
+	int rel_x;
+	int rel_y;
+}point;
+
+void print_grid(int w, int h, point num_pts[MAX_POINTS], int pts_index);
+
+point* sort_arr(point num_pts[], int pts_index);
 
 void grid_mode(void);
 
